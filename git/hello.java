75664e3cd80ed08fca0b280bbcd0ff7fcffe5d0b
“ditiss98-code” <sai@gmail.com>
2025-11-08 16:18:44 +0530

    fix bug 2

--- git/hello.java
+++ git/hello.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, World this is sai!");
+        System.out.println("Hello, World this is sai my name is prasad!");
     }
 }
